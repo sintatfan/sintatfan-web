a2f67df3d928ff54dc725b804ca6b7061aa95de1
indzztat <sintat@sintat.fan>
2022-11-24 00:45:38 +0800

    Footer basic structure

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -1,9 +1,18 @@
-import styles from "./footer.module.css";
-
 export default function PageFooter() {
     return (
-        <footer className={styles.footer}>
-            Page Footer
+        <footer className="container relative py-16 space-y-3">
+            {/* Main content */}
+            <div className="flex flex-col items-center">
+                <img src="/logo.svg" alt="" className="w-10 grayscale" />
+                <div className="text-gray-400 text-xs">
+                    Made with Next.js &amp; Tailwind CSS.
+                </div>
+            </div>
+
+            {/* Decoration */}
+            <div className="absolute -bottom-16 -right-1/4">
+                <img src="/bg_4pills.svg" alt="" />
+            </div>
         </footer>
     );
 }
