1f3e1c6b6aa9bdde8e35a4c0fc023e7b61412510
Tat <sintat@sintat.fan>
2022-11-24 11:23:36 +0800

    Footer - Remove unnecessary horizontal padding

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -2,7 +2,7 @@ export default function PageFooter() {
     return (
         <footer className="relative container">
             {/* Main content */}
-            <div className="flex flex-col items-center space-y-3 px-5 py-16">
+            <div className="flex flex-col items-center space-y-3 py-16">
                 <img src="/images/logo.svg" alt="" className="w-10 grayscale" />
                 <div className="text-gray-400 text-xs">
                     Made with Next.js &amp; Tailwind CSS.
