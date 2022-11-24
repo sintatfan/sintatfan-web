28c11d9d6b0d99a1c2fb9508b0896c5cf65893a6
Tat <sintat@sintat.fan>
2022-11-24 20:21:32 +0800

    About - compose layout and style of biography

--- components/home/about.jsx
+++ components/home/about.jsx
@@ -1,12 +1,32 @@
 import {SectionTitle} from "../title";
 
+function Biography() {
+    return (
+        <div className="flex items-center space-x-5 py-10 mb-12">
+            <div className="flex-none w-72 text-center">
+                <img src="/images/profilepic.jpg" srcSet="/images/profilepic@2x.jpg 2x" alt="My profile picture"
+                     className="rounded-full mx-auto mb-4" />
+
+                <div className="text-2xl font-bold">Sin-Tat Fan</div>
+                <div className="font-light">
+                    <abbr title="Master in Design" className="no-underline">MDes</abbr> (Interaction Design)<br />
+                    <abbr title="Bachelor of Science" className="no-underline">BSc</abbr> in Computer Science
+                </div>
+            </div>
+            <div className="flex-auto max-w-3xl">
+                <p className="text-5xl mb-3">Computer Science is <br />an <span className="text-gradient font-bold">art of problem solving</span>.</p>
+                <p className="text-lg">I am a passionate and competent Software Developer with over 9 years of experience in web applications development. With my knowledge in Computer Science and Interaction Design, as well as my solid work experience, I am eager to help companies build elegant and well-designed digital solutions that solve problems, engage with people and boost productivity.</p>
+            </div>
+        </div>
+    );
+}
+
 export default function AboutSection() {
     return (
         <div className="container-md">
             <SectionTitle>About</SectionTitle>
 
-            <p>Computer Science is an <span className="text-gradient font-bold">art of problem solving</span>.</p>
-            <p className="text-lg">I am a passionate and competent Software Developer with 9+ years of experience in web applications development. With my knowledge in Computer Science and Interaction Design, as well as my solid work experience, I am eager to help companies build elegant and well-designed digital solutions that solve problems, engage with people and boost productivity.</p>
+            <Biography />
 
             <h2 className="h2">What do I do?</h2>
             <h3 className="h3">I design.</h3>
