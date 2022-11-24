7886c421ef364e927bd931f8c2a73e3e34852e52
Tat <sintat@sintat.fan>
2022-11-24 18:23:21 +0800

    Migrate to traditional pages based structure from the experimental app/ directory

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,15 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import LandingSection from "../components/home/landing";
 import AboutSection from "../components/home/about";
 import WorkSection from "../components/home/portfolio";
 import ContactSection from "../components/home/contact";
 
 export default function Home() {
-  return (
-    <div>
-      <LandingSection />
-      <AboutSection />
-      <WorkSection />
-      <ContactSection />
-    </div>
-  )
+    return (
+        <div>
+            <LandingSection />
+            <AboutSection />
+            <WorkSection />
+            <ContactSection />
+        </div>
+    )
 }
