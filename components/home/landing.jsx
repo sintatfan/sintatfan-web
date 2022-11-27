7945d95f2944caa60d202c2f40e8dec92a13e393
Tat <sintat@sintat.fan>
2022-11-27 14:57:47 +0800

    Landing - prepare content structure

--- components/home/landing.jsx
+++ components/home/landing.jsx
@@ -1,7 +1,41 @@
 export default function LandingSection() {
     return (
         <div className="container">
-            Landing
+            <div className="grid grid-cols-2 gap-5 items-center">
+                <LandingContent />
+                <LandingGraphic />
+            </div>
+        </div>
+    );
+}
+
+function LandingContent() {
+    return (
+        <div>
+            <div className="mb-6">
+                <div className="font-bold text-4xl">
+                    Hello, I&apos;m <span className="text-gradient">Sin-Tat</span>.
+                </div>
+                <div className="font-light">
+                    <span className="text-2xl">Fullstack Developer</span> - <span>Frontend Dev | Backend Dev | UX Architect</span>
+                </div>
+            </div>
+
+            <div className="text-5xl leading-tight">
+                I <span className="text-gradient font-bold">design</span>, <span
+                className="text-gradient font-bold">develop</span> and <span
+                className="text-gradient font-bold">optimize</span> software that solve problems and delight people.
+            </div>
+        </div>
+    );
+}
+
+function LandingGraphic() {
+    return (
+        <div>
+            <svg width={700} height={700}>
+                <rect x={0} y={0} width={700} height={700} fill="#888888"></rect>
+            </svg>
         </div>
     );
 }
