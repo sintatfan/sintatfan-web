6d92f57b01847ada83b0033b518d0c53e3ce0802
indzztat <sintat@sintat.fan>
2022-11-27 22:54:09 +0800

    Header - collapse on scroll down

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -1,9 +1,21 @@
 import styles from "./header.module.css";
 import Navigation from "./navigation";
+import {useEvent} from "react-use";
+import {useState} from "react";
 
 export default function PageHeader() {
+    const [isGlassy, setIsGlassy] = useState(false);
+    const onPageScroll = function (e) {
+        if (window.scrollY >= 64 && !isGlassy) {
+            setIsGlassy(true);
+        } else if (window.scrollY < 64 && isGlassy) {
+            setIsGlassy(false);
+        }
+    };
+    useEvent('scroll', onPageScroll);
+
     return (
-        <header className={`${styles.header} bg-body/80 backdrop-blur-xl fixed top-0 left-0 right-0 z-50`}>
+        <header className={`${styles.header} fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isGlassy ? `${styles.headerGlassy} bg-body/80 backdrop-blur-xl` : ''}`}>
             <div className="container h-full flex items-center justify-between">
                 {/* Logo */}
                 <a href="#top">
