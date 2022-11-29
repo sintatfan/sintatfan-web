98fe0becb5760f055ac86108ddf1cf902ae82eb6
Tat <sintat@sintat.fan>
2022-11-29 21:03:57 +0800

    Custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+    return (
+        <div className="container text-center my-32 min-h-[calc(100vh-600px)]">
+            <h1 className="text-5xl font-bold mb-4">
+                <span className="text-gradient">404 Not Found</span>
+            </h1>
+            <div className="mb-8">
+                Oh no! 😖 The page that you were looking for doesn&apos;t exist.
+            </div>
+            <Link href={'/'} className="btn btn-outline btn-md">Back to Home</Link>
+        </div>
+    );
+}
