28ecaeb1e7279e17dd5903ff2ac18f343fa658c3
Tat <sintat@sintat.fan>
2022-11-29 21:04:24 +0800

    Add padding to 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFoundPage() {
     return (
-        <div className="container text-center my-32 min-h-[calc(100vh-600px)]">
+        <div className="container px-4 text-center my-32 min-h-[calc(100vh-600px)]">
             <h1 className="text-5xl font-bold mb-4">
                 <span className="text-gradient">404 Not Found</span>
             </h1>
