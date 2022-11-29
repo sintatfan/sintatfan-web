091c8a9e4a96dde13db47345308a12d56a1b60a4
Tat <sintat@sintat.fan>
2022-11-29 20:38:46 +0800

    About - separate design icons into two files

--- components/home/about.jsx
+++ components/home/about.jsx
@@ -16,7 +16,7 @@ export default function AboutSection() {
             <h2 className="h2 mb-8">What do I do?</h2>
 
             <div className="mb-24">
-                <AboutSubsection heading="I design." iconSrc="/images/about_design.svg" lightPos="left">
+                <AboutSubsection heading="I design." icon={<DesignIcon />} lightPos="left">
                     <p>Morbi mauris dolor, lacinia id elit eget, lacinia mattis urna. Proin luctus, arcu a pulvinar condimentum, tortor lectus fermentum eros, a varius nisi ex quis orci. Aenean placerat congue auctor. Nulla ullamcorper egestas eros. Aenean nec porttitor arcu.</p>
                 </AboutSubsection>
 
@@ -31,5 +31,15 @@ export default function AboutSection() {
 
             <SubsectionShare />
         </div>
+    );
+}
+
+function DesignIcon() {
+    return (
+        <svg width="240" height="240" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg"
+             className="w-full">
+            <image href="/images/about_design_system.svg" height="175" width="175" x="-8" y="8" />
+            <image href="/images/about_design_flow.svg" height="175" width="175" x="75" y="60" />
+        </svg>
     );
 }
--- components/home/about_subsection.jsx
+++ components/home/about_subsection.jsx
@@ -1,11 +1,11 @@
 import {LightDecoration} from "../decoration";
 
-export default function AboutSubsection({iconSrc, heading, children, lightPos}) {
+export default function AboutSubsection({icon, iconSrc, heading, children, lightPos}) {
     return (
         <div className="sm:flex items-center sm:space-x-8 md:space-x-0 py-8 md:py-2">
             <div className="flex-none mb-6 sm:mb-0 sm:w-1/3 md:w-5/12 flex justify-center">
                 <div className="relative">
-                    <img src={iconSrc} alt="" />
+                    {iconSrc ? <img src={iconSrc} alt="" /> : icon}
                     <LightDecoration x={lightPos === 'right' ? '-right-3/4' : '-left-3/4'} y="-bottom-36" extraClass="scale-50" />
                 </div>
             </div>
