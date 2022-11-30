bcd1ebea22a7b6b63458dff8e0a6e98f03f58cc5
Tat <sintat@sintat.fan>
2022-11-30 15:51:50 +0800

    Landing graphic - Fine-tuning across devices

--- components/home/landing_content.jsx
+++ components/home/landing_content.jsx
@@ -1,6 +1,6 @@
 export function LandingContent() {
     return (
-        <div className="py-16 lg:py-0">
+        <div className="py-12 lg:py-0">
             <div className="mb-6">
                 <div className="font-bold text-4xl">
                     Hello, I&apos;m <span className="text-gradient">Sin-Tat</span>.
@@ -10,7 +10,7 @@ export function LandingContent() {
                 </div>
             </div>
 
-            <div className="text-4xl xl:text-5xl leading-tight xl:leading-tight">
+            <div className="text-4xl 2xl:text-5xl leading-tight 2xl:leading-tight">
                 I <span className="text-gradient font-bold">design</span>, <span
                 className="text-gradient font-bold">develop</span> and <span
                 className="text-gradient font-bold">optimize</span> software that solve problems and delight people.
--- components/home/landing_graphic.jsx
+++ components/home/landing_graphic.jsx
@@ -1,7 +1,7 @@
 export function LandingGraphic() {
     return (
         <div>
-            <svg width={860} height={700}>
+            <svg viewBox="0 0 860 700" className="w-full lg:w-[550px] lg:h-[448px] xl:w-[660px] xl:h-[537px] 2xl:w-[860px] 2xl:h-[700px]">
                 <style jsx>{`
                 .landing-graphic-line {
                     fill: none;
@@ -13,22 +13,22 @@ export function LandingGraphic() {
                   animation: line1 14s infinite alternate;
                 }
                 .landing-graphic-line-2 {
-                  animation: line1 12s infinite alternate;
+                  animation: line1 10s infinite alternate;
                 }
                 
                 @keyframes line1 {
                     from {stroke-dashoffset: 0;}
-                    to {stroke-dashoffset: -430.744;}
+                    to {stroke-dashoffset: -430;}
                 }
                 @keyframes line2 {
                     from {stroke-dashoffset: 0;}
-                    to {stroke-dashoffset: -521.277;}
+                    to {stroke-dashoffset: -520;}
                 }
                 `}</style>
                 <defs>
                     <radialGradient id="shadow-gradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                        <stop stopColor="#666666" stopOpacity="0.76"/>
-                        <stop offset="1" stopColor="white" stopOpacity="0"/>
+                        <stop stopColor="#666666" stopOpacity="0.5"/>
+                        <stop offset="1" stopColor="#666666" stopOpacity="0"/>
                     </radialGradient>
                     <linearGradient id="line-gradient" x1="0%" y1="100%" x2="100%" y2="0%">
                         <stop stopColor="#FFC345"/>
@@ -47,25 +47,25 @@ export function LandingGraphic() {
 
                 {/* Line */}
                 <g transform="translate(320 380)">
-                    <path d="M10 169.5C10 169.5 176 169.5 254.5 169.5C297.5 169.5 294.5 146 294.5 118.755C294.5 72 294.5 10 294.5 10"
+                    <path d="M10 169C10 169 176 169 254 169C297 169 294 146 294 118C294 72 294 10 294 10"
                           stroke="url(#line-gradient)" strokeDasharray="110 30 20 30 37 30 20 30 49 30 20 30"
                           className="landing-graphic-line landing-graphic-line-1" />
                 </g>
                 <g transform="translate(320 380)">
-                    <path d="M0 209.5C0 209.5 176 209.5 274.5 209.5C339.5 209.5 339.5 146 339.5 118.755C339.5 72 339.5 0 339.5 0"
+                    <path d="M0 209C0 209 176 209 274 209C339 209 339 146 339 118C339 72 339 0 339 0"
                           stroke="url(#line-gradient)" strokeDasharray="48 30 117 30 92 30 73 30 40 30 5 30 60 30"
                           className="landing-graphic-line landing-graphic-line-2" />
                 </g>
 
                 {/* System Design Diagram */}
                 <g transform="translate(-15 340)">
-                    <ellipse cx={195} cy={290} rx={190} ry={65} fill="url(#shadow-gradient)"/>
+                    <ellipse cx={195} cy={295} rx={180} ry={40} fill="url(#shadow-gradient)"/>
                     <image href="/images/about_design_system.svg" x={0} y={0} width={380} height={380} filter="url(#shadow)"></image>
                 </g>
 
                 {/* Web Browser Console */}
                 <g transform="translate(475 80)">
-                    <ellipse cx={195} cy={290} rx={190} ry={65} fill="url(#shadow-gradient)"/>
+                    <ellipse cx={195} cy={295} rx={180} ry={65} fill="url(#shadow-gradient)"/>
                     <image href="/images/about_optimize.svg" x={0} y={0} width={380} height={380} filter="url(#shadow)"></image>
                 </g>
             </svg>
