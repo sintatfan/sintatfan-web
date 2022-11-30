1b1f484b1039bf282db4d21bdb95de87c55c7081
Tat <sintat@sintat.fan>
2022-11-30 15:38:37 +0800

    Landing graphic - Correct SVG attribute to JSX-compatible syntax

--- components/home/landing_graphic.jsx
+++ components/home/landing_graphic.jsx
@@ -27,17 +27,17 @@ export function LandingGraphic() {
                 `}</style>
                 <defs>
                     <radialGradient id="shadow-gradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                        <stop stop-color="#666666" stop-opacity="0.76"/>
-                        <stop offset="1" stop-color="white" stop-opacity="0"/>
+                        <stop stopColor="#666666" stopOpacity="0.76"/>
+                        <stop offset="1" stopColor="white" stopOpacity="0"/>
                     </radialGradient>
                     <linearGradient id="line-gradient" x1="0%" y1="100%" x2="100%" y2="0%">
-                        <stop stop-color="#FFC345"/>
-                        <stop offset="0.5" stop-color="#F29516"/>
-                        <stop offset="1" stop-color="#FFC345"/>
+                        <stop stopColor="#FFC345"/>
+                        <stop offset="0.5" stopColor="#F29516"/>
+                        <stop offset="1" stopColor="#FFC345"/>
                     </linearGradient>
                 </defs>
                 <filter id="shadow">
-                    <feDropShadow dx="0" dy="2" stdDeviation="4" flood-color="#999999" flood-opacity="0.12" />
+                    <feDropShadow dx="0" dy="2" stdDeviation="4" floodColor="#999999" floodOpacity="0.12" />
                 </filter>
 
                 <g transform="translate(45 0)">
@@ -48,12 +48,12 @@ export function LandingGraphic() {
                 {/* Line */}
                 <g transform="translate(320 380)">
                     <path d="M10 169.5C10 169.5 176 169.5 254.5 169.5C297.5 169.5 294.5 146 294.5 118.755C294.5 72 294.5 10 294.5 10"
-                          stroke="url(#line-gradient)" stroke-dasharray="110 30 20 30 37 30 20 30 49 30 20 30"
+                          stroke="url(#line-gradient)" strokeDasharray="110 30 20 30 37 30 20 30 49 30 20 30"
                           className="landing-graphic-line landing-graphic-line-1" />
                 </g>
                 <g transform="translate(320 380)">
                     <path d="M0 209.5C0 209.5 176 209.5 274.5 209.5C339.5 209.5 339.5 146 339.5 118.755C339.5 72 339.5 0 339.5 0"
-                          stroke="url(#line-gradient)" stroke-dasharray="48 30 117 30 92 30 73 30 40 30 5 30 60 30"
+                          stroke="url(#line-gradient)" strokeDasharray="48 30 117 30 92 30 73 30 40 30 5 30 60 30"
                           className="landing-graphic-line landing-graphic-line-2" />
                 </g>
 
